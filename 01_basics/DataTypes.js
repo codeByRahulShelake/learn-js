4141324e84ed4b67b342241e6c6c106ffe9a5195
Rahul Shelake <rahulshelake.connect@gmail.com>
2024-11-21 04:10:40 +0000

    variables and datatypes

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,13 @@
+const accountId = 1234;
+var accountName = "Rahul Shelake";
+let accountEmail = null;
+let accountCity;
+let accountState = true;
+
+
+
+console.log(`Account Id : ${typeof accountId}
+Account Name : ${typeof accountName}
+Account Email : ${typeof accountEmail}
+Account City : ${typeof accountCity}
+Account State : ${typeof accountState}`);
---
+++ 01_basics/Variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234;
+var accountName = "Rahul Shelake";
+let accountEmail = "rahulshelake.connect@gmail.com";
+accountCity = "Kolhapur";
+let accountState;
+
+console.table([accountId, accountName, accountEmail, accountCity]);
+
+console.log(`Account Id : ${accountId}
+Account Name : ${accountName}
+Account Email : ${accountEmail}
+Account City : ${accountCity}
+Account State : ${accountState}`);
