7b7052eb6a5e95b3961ee53b84630555a97f4fbc
Rahul Shelake <rahulshelake.connect@gmail.com>
2024-11-21 05:59:53 +0000

    Done number methods and math methods

---
+++ 01_basics/NumberOperation.js
@@ -0,0 +1,13 @@
+let num = 45
+console.log(num.toFixed(2))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random() * 10 + 1)) 
+
+let min = 1000;
+let max = 9999;
+console.log(Math.floor(Math.random() * (max - min + 1) + min)) 
+
+let balance = 10000000
+console.log(balance.toLocaleString())
+console.log(balance.toLocaleString('en-In'))
