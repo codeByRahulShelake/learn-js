9e082d184cb795a161d3e06dc02f29aaaa54433a
Rahul Shelake <rahulshelake.connect@gmail.com>
2024-11-21 17:31:55 +0530

    Learned about objects joining and about Object class methods

--- 01_basics/ObjectDemo.js
+++ 01_basics/ObjectDemo.js
@@ -33,3 +33,28 @@ Logged in : ${this.isLoggedIn}`)
 
 console.log(myObj.display())
 console.log(myObj.displayInfo())
+
+const obj1 = { 1 : "a", 2 : "b"}
+const obj2 = { 1 : "a", 2 : "b"}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3);
+
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}  // combining two objects with spread operator
+console.log(obj3);
+
+
+const userInfo = { // object inside object
+    name : {
+        firstName : "Rahul",
+        lastName : "Shelake"
+    },
+    id : 101 
+}
+
+console.log(userInfo.name.firstName);
+
+console.log(Object.keys(myObj)); // display each key in array format
+console.log(Object.values(myObj));// display each value in array format
+console.log(Object.entries(myObj));// display each key-value pair in array format
