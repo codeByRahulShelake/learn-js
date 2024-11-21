6e535867e1240e3b5ced4b72ba1e969b6c6e9054
Rahul Shelake <rahulshelake.connect@gmail.com>
2024-11-21 19:36:56 +0530

    learned arrow functions

---
+++ 01_basics/02_arrowFunstions.js
@@ -0,0 +1,21 @@
+const display = () => {
+    console.log("Hello");
+}
+// display()
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(add(1,3));
+const newAdd = (num1, num2) => num1 + num2
+
+console.log(newAdd(454,18));
+
+const displayMsg = () => "Hello"
+
+console.log(displayMsg());
+
+const returnObj = () => ({username : "Rahul"})
+
+console.log(returnObj());
