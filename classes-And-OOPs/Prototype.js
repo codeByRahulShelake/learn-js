50ba41db2f1247cb14bcd731162ed9b4020d6b05
Rahul Shelake <rahulshelake.connect@gmail.com>
2024-11-28 19:32:57 +0530

    learned in deep about prototypes

---
+++ classes-And-OOPs/Prototype.js
@@ -0,0 +1,61 @@
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.rahul = function(){
+    console.log(`Rahul is present in all objects`);
+}
+
+Array.prototype.heyRahul = function(){
+    console.log(`Rahul says hello`);
+}
+
+heroPower.rahul()
+myHeros.rahul()
+myHeros.heyRahul()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "Rahul Shelake     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"Jay Shri Ram".trueLength()
+"iceTea".trueLength()
