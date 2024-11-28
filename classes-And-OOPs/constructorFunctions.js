25ab37533c9802ae130fc0ad87f5a690ea068036
Rahul Shelake <rahulshelake.connect@gmail.com>
2024-11-28 17:02:14 +0530

    learned about constructor functions

---
+++ classes-And-OOPs/constructorFunctions.js
@@ -0,0 +1,12 @@
+function User(name, age, address){
+    this.name = name
+    this.age = age
+    this.address = address
+}
+
+const rahul = new User('Rahul Shelake', 22, 'Kolhapur')
+const rohan = new User('Rohan Patil', 18, 'Satara')
+
+console.log(rahul);
+console.log(rohan);
+
